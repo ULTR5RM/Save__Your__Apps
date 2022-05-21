553008fa2cf26888fedb2d7151a69de0c880e36e
ULTR5RM <96973326+ULTR5RM@users.noreply.github.com>
2022-05-21 14:02:11 +0700

    Добавить README.md.

---
+++ Save__Your__Apps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Save__Your__Apps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
